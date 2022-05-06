e98a962317996674abfe8e434f8988748423c89a
VR-Blackbird <venkataraman1503@gmail.com>
2022-05-06 21:11:30 +0530

    Number types franctional

--- numbers.cpp
+++ numbers.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -10,6 +11,18 @@ int main()
     int negative_number{-10};
     signed int signed_number{10};
     unsigned int unsigned_number{1};
+
+    // Fractions
+    double num1 = -10;
+    double num2 = 23;
+    double random{};
+    double div = num1 / num2;
+    double inf = num1 / random;
+    cout << "Infinity is : " << inf << endl;
+
+    cout << "The div output is : " << div << endl;
+
+    //
     cout << "The different numbers are: " << dec_number << endl
          << oct_number << endl
          << bin_number << endl
