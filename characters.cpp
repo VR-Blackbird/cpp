c29168cde87bd665870dddfa9ccf49c4a28adb59
VR-Blackbird <venkataraman1503@gmail.com>
2022-05-06 22:59:14 +0530

    characters

---
+++ characters.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char z = 'z';
+    char gibb = 195;
+    cout << static_cast<int>(z) << endl;
+    cout << "Gibberish is :" << gibb << endl;
+}
