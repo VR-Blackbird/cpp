43ff169f82db0952d6106a4439f06e0be6d70abb
VR-Blackbird <venkataraman1503@gmail.com>
2022-05-17 20:29:42 +0530

    Range based for loops

--- Basic_for.cpp
+++ Basic_for.cpp
@@ -13,4 +13,17 @@ int main()
     cout << endl;
     for (size_t i{1}; i <= Count; ++i) // Curly braces can be omitted if we have only one statement in loop body
         cout << pow(i, 3) << endl;
+
+    // Range based for loops
+    int values[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (int value : values)
+    {
+        cout << value << endl;
+    }
+
+    cout << endl;
+    for (auto value : {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    {
+        cout << value << endl;
+    }
 }
