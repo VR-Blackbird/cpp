070052a8cbfe34451e37e3a2bc65f7287dcfa4d8
VR-Blackbird <venkataraman1503@gmail.com>
2022-06-10 23:26:59 +0530

    Pass by value

---
+++ functions/pass_by_value.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void age_copy(int age);
+
+int main()
+{
+    int age{21};
+    cout << "Before function call " << age << endl;
+    age_copy(age);
+    cout << "After function call " << age << endl; 
+    return 0;
+}
+
+void age_copy(int age)
+{
+
+    ++age;
+    cout << "During function call " << age << endl;
+}
