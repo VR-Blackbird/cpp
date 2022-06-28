d58a59e0a4d4ad0571a040c2c109cea600046e4e
Venkataraman <92672198+VR-Blackbird@users.noreply.github.com>
2022-06-28 23:09:48 +0530

    Extra clarification

--- function_template/default_arguments.cpp
+++ function_template/default_arguments.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-template <typename ReturnType = double, typename T, typename P>
+template <typename ReturnType = int, typename T, typename P>
 ReturnType maximum(T a, P b)
 {
     return (a > b) ? a : b;
@@ -21,6 +21,8 @@ int main()
     auto result = minimum<int, double, int>(a, b);
     std::cout << "result : " << result << std::endl;
     std::cout << "sizeof(result) : " << sizeof(result) << std::endl; // 4
+    std::cout << maximum(a, b) << std::endl;                         // Default return type int
+    std::cout << maximum<double, int, double>(a, b) << std::endl;   //Explicit return type double
 
     return 0;
 }
