2667c5b0f6bf3ac33610dffbac36d964c7bd1caf
Venkataraman <92672198+VR-Blackbird@users.noreply.github.com>
2022-07-06 16:40:14 +0530

    exercise 49

---
+++ Exercises/exercise49/exercise.cpp
@@ -0,0 +1,32 @@
+#include "exercise.h"
+#include <iostream>
+
+Point::Point(double x, double y)
+{
+    m_x = x;
+    m_y = y;
+}
+
+double Point::x()
+{
+    return m_x;
+}
+double Point::y()
+{
+    return m_y;
+}
+Point &Point::set_x(double val)
+{
+    this->m_x = val;
+    return *this;
+}
+Point &Point::set_y(double val)
+{
+    this->m_y = val;
+    return *this;
+}
+
+void print_point(Point &p)
+{
+    std::cout << "Point[x : " << p.x() << ", y : " << p.y() << "]";
+}
---
+++ Exercises/exercise49/exercise.h
@@ -0,0 +1,26 @@
+#ifndef EXERCISE_H
+#define EXERCISE_H
+
+class Point
+{
+public:
+    // methods
+    Point() = default;
+    Point(double x, double y);
+
+    double x();
+
+    double y();
+
+    Point &set_x(double val);
+
+    Point &set_y(double val);
+    // member variables
+private:
+    double m_x{1};
+    double m_y{1};
+};
+
+void print_point(Point &p);
+
+#endif // _EXERCISE_H
