f326e34d786af67de788321404877b124deebdbc
Venkataraman <92672198+VR-Blackbird@users.noreply.github.com>
2022-07-08 23:28:07 +0530

    removed unwanted comments

--- class2/getterAsSetter/main.cpp
+++ class2/getterAsSetter/main.cpp
@@ -16,15 +16,5 @@ int main()
     const Dog dog2("Ffy", "She", 21);
     std::cout << dog2.get_age() << std::endl;
 
-    // dog1.set_age(10); Compiler error
-
-    // dog1.set_name("Milou"); // Compiler error
-
-    // Pointer to non const
-    // Dog * dog_ptr = &dog1;
-
-    // Non const reference
-    // Dog& dog_ref = dog1;
-
     return 0;
 }
